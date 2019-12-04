2f4ea18bc43b1ee8b382d8330abaf231169c9f38
Sebastian Arming <sebastian@arming.net>
2019-12-04 11:45:24 +0100

    cleanup

--- propagation.py
+++ propagation.py
@@ -33,7 +33,7 @@ def neighbors(A, node):
     return A.indices[A.indptr[node]:A.indptr[node + 1]]
 
 
-def propagate(A, start, prob, depth=1):
+def node_propagate(A, start, prob, depth=1):
     # if prob is scalar treat as uniform probability (except for start node)
     if isinstance(prob, float):
         prob = [1 if n == start else prob for n in range(A.shape[0])]
@@ -50,8 +50,6 @@ def propagate(A, start, prob, depth=1):
                 new_leaves.extend(children)
                 tree.add_star([node] + children)
         leaves = new_leaves
-        if not leaves:
-            break
     return tree
 
 
@@ -59,9 +57,10 @@ if __name__ == "__main__":
     for i in range(1, 2):
         graph = read.metis(f'1K/graphs/{i}.metis')
         # print(graph.number_of_nodes())
-        # A = graphs.uniform_adjacency(graph, 0.05)
-        A = nx.to_scipy_sparse_matrix(graph)
-        tree = propagate(A, 0, 0.1, 10)
+        A = graphs.uniform_adjacency(graph, 0.1)
+        # A = nx.to_scipy_sparse_matrix(graph)
+        # tree = node_propagate(A, 0, 0.1, 10)
+        tree = edge_propagate(A, 0, 10)
         print(tree.number_of_nodes())
         # print(nx.nx_pydot.to_pydot(tree))
         nx.draw(tree)
