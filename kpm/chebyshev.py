75d1cb69d977a892169882eeac7aa4dcd1f5f924
Sebastian Arming <sebastian@arming.net>
2021-11-15 17:30:53 +0100

    cythonize chebyshev.py

--- kpm/chebyshev.py
+++ kpm/chebyshev.py
@@ -1,14 +1,15 @@
+import cython
 import numpy as np
 
 
-def random_vector(n):
+def random_vector(n: cython.int):
     """Return normalized dimension n random vector."""
     # return np.random.choice([-1, 1], n)
     v = 2 * np.random.rand(n) - 1
     return v / np.linalg.norm(v)
 
 
-def sample(A, coef, v, return_history=False):
+def sample(A, coef, v) -> cython.int:
     """Return sum_i ( coef_i * v * T_i(A) @ v ) = v * coef(A) @ v."""
     # T_0(x) = 1
     # T_1(x) = x
@@ -16,8 +17,7 @@ def sample(A, coef, v, return_history=False):
     # w_i = T_{n-i} * v (we start at n=2)
     w_2 = v
     w_1 = A @ v
-    sample = coef[0] * (v @ w_2) + coef[1] * (v @ w_1)
-    history = []
+    sample: cython.int = coef[0] * (v @ w_2) + coef[1] * (v @ w_1)
     for c in coef[2:]:
         w_0 = A @ w_1
         w_0 *= 2
@@ -25,13 +25,17 @@ def sample(A, coef, v, return_history=False):
         sample += c * (v @ w_0)
         w_2 = w_1
         w_1 = w_0
-        if return_history:
-            history.append(sample)
-    return history if return_history else sample
+    return sample
 
 
-def estimator(A, coef, samples):
+def estimator(A, coef, samples: cython.int) -> cython.float:
     """Estimate trace of coef(A) with samples via Hutchinson's estimator."""
+
+    if cython.compiled:
+        print("Yep, I'm compiled.")
+    else:
+        print("Just a lowly interpreted script.")
+
     assert len(coef) > 1
     n = A.shape[0]
     s = sum(sample(A, coef, random_vector(n)) for _ in range(samples))
--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+from Cython.Build import cythonize
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        ext_modules=cythonize(
+            "kpm/chebyshev.py", compiler_directives={'language_level': "3"}, annotate=True
+        )
+    )
